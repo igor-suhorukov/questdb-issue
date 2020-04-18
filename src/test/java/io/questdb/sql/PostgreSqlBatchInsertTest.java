5b40028ab2615dfeefcdaa867e3304b259b6650a
igor-suhorukov <igor_suhorukov@mail.ru>
2020-04-18 13:48:00 +0300

    Reproduce issue with executeBatch(). executeLargeBatch() works fine

--- src/test/java/io/questdb/sql/PostgreSqlBatchInsertTest.java
+++ src/test/java/io/questdb/sql/PostgreSqlBatchInsertTest.java
@@ -1,6 +1,7 @@
 package io.questdb.sql;
 
 import io.questdb.ServerMain;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
@@ -11,9 +12,13 @@ import java.sql.PreparedStatement;
 import java.sql.Statement;
 
 public class PostgreSqlBatchInsertTest {
-    @Test
-    void batchInsert(@TempDir Path tempDir) throws Exception{
+    @BeforeAll
+    static void init(@TempDir Path tempDir) throws Exception{
         ServerMain.main(new String[]{"-d", tempDir.toString()});
+    }
+
+    @Test
+    void batchInsertWithTransaction() throws Exception{
         try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:8812/", "admin", "quest")){
             try (Statement statement = connection.createStatement()){
                 statement.executeUpdate("create table test(id long,val int)");
@@ -35,4 +40,49 @@ public class PostgreSqlBatchInsertTest {
             }
         }
     }
+
+    @Test
+    void largeBatchInsertMethod() throws Exception{
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:8812/", "admin", "quest")){
+            try (Statement statement = connection.createStatement()){
+                statement.executeUpdate("create table test_large_batch(id long,val int)");
+            }
+            try (PreparedStatement batchInsert = connection.prepareStatement("insert into test_large_batch(id,val) values(?,?)")){
+                batchInsert.setLong(1,0L);
+                batchInsert.setInt(2, 1);
+                batchInsert.addBatch();
+                batchInsert.setLong(1,1L);
+                batchInsert.setInt(2, 2);
+                batchInsert.addBatch();
+                batchInsert.setLong(1,2L);
+                batchInsert.setInt(2, 3);
+                batchInsert.addBatch();
+                batchInsert.clearParameters();
+                long[] longs = batchInsert.executeLargeBatch();
+            }
+        }
+    }
+
+    @Test
+    void regularBatchInsertMethod() throws Exception{
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:8812/", "admin", "quest")){
+            try (Statement statement = connection.createStatement()){
+                statement.executeUpdate("create table test_batch(id long,val int)");
+            }
+            try (PreparedStatement batchInsert = connection.prepareStatement("insert into test_batch(id,val) values(?,?)")){
+                batchInsert.setLong(1,0L);
+                batchInsert.setInt(2, 1);
+                batchInsert.addBatch();
+                batchInsert.setLong(1,1L);
+                batchInsert.setInt(2, 2);
+                batchInsert.addBatch();
+                batchInsert.setLong(1,2L);
+                batchInsert.setInt(2, 3);
+                batchInsert.addBatch();
+                batchInsert.clearParameters();
+                int[] longs = batchInsert.executeBatch();
+            }
+        }
+    }
+
 }
